3fd924e4d5c54be577a19e07ed01617826bf816e
Etrik Patricella <nominal@gmail.com>
2025-06-12 16:00:56 -0400

    support lists

--- utils.py
+++ utils.py
@@ -9,37 +9,48 @@ def parse_hcl_file(file_path):
 
 def find_provider_default_tags(data):
     """
-    Looks for provider blocks with default_tags
+    Looks for provider blocks with default_tags.
+    Supports both list and dict formats.
     """
     provider_tags = {}
 
-    providers = data.get("provider", {})
-    for provider_name, blocks in providers.items():
-        if isinstance(blocks, list):
-            for block in blocks:
-                if "default_tags" in block:
-                    provider_tags[provider_name] = block["default_tags"].get("tags", {})
+    if "provider" in data:
+        for provider_block in data["provider"]:
+            for provider_name, provider_config in provider_block.items():
+                if isinstance(provider_config, list):
+                    for conf in provider_config:
+                        if "default_tags" in conf:
+                            tags = conf["default_tags"].get("tags", {})
+                            if tags:
+                                provider_tags[provider_name] = tags
+                elif isinstance(provider_config, dict):
+                    if "default_tags" in provider_config:
+                        tags = provider_config["default_tags"].get("tags", {})
+                        if tags:
+                            provider_tags[provider_name] = tags
     return provider_tags
 
 def find_resources_missing_tags(data, provider_tags):
     """
-    Finds resources that don't have tags and don't inherit them via provider
+    Finds resources that don't have tags and don't inherit them via provider.
+    Supports both list and dict formats.
     """
     untagged = []
 
-    resources = data.get("resource", {})
-    for res_type, res_list in resources.items():
-        for res_name, res_body in res_list.items():
-            # AWS-only tag check for now
-            if "tags" not in res_body:
-                inherits = any(
-                    prov for prov, tags in provider_tags.items()
-                    if prov.startswith("aws") and tags
-                )
-                if not inherits:
-                    untagged.append({
-                        "type": res_type,
-                        "name": res_name
-                    })
+    if "resource" in data:
+        for resource_block in data["resource"]:
+            for res_type, instances in resource_block.items():
+                for res_name, res_config in instances.items():
+                    has_tags = "tags" in res_config
+                    inherits_tags = any(
+                        prov for prov, tags in provider_tags.items()
+                        if prov.startswith("aws") and tags
+                    )
+                    if not has_tags and not inherits_tags:
+                        untagged.append({
+                            "type": res_type,
+                            "name": res_name
+                        })
+
     return untagged
 
